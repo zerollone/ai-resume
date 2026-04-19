8d5444b9d109df6d3f578a16661376ae930d2507
zerollone <2294869408@qq.com>
2026-04-19 12:26:42 +0800

    feat: 添加用户管理

--- src/main/java/com/ai/resume/config/SaTokenConfig.java
+++ src/main/java/com/ai/resume/config/SaTokenConfig.java
@@ -28,7 +28,8 @@ public class SaTokenConfig {
                             "/swagger-resources/**",
                             "/webjars/**",
                             "/v3/api-docs/**",
-                            "/favicon.ico");
+                            "/favicon.ico"
+                    );
         }
     }
 }
